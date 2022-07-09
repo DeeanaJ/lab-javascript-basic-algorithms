db47a39d18c144fc482a0eacb6ba8d42f4840b68
Diana Jurca <diana.roxana.lazar@gmail.com>
2022-07-09 17:26:17 +0200

    lab ready

--- js/index.js
+++ js/index.js
@@ -20,3 +20,24 @@ else if(hacker1.length < hacker2.length) {
 else console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`)
 
 // Iteration 3: Loops
+let driverCapsSpaces= ""
+for (i in hacker1){
+    driverCapsSpaces += hacker1[i] + ' '
+}
+console.log(driverCapsSpaces.toUpperCase())
+
+//3.2 reversed name
+let revName= ""
+for (i=hacker2.length-1; i>=0; i--){
+    revName += hacker2[i] 
+}
+console.log(revName)
+
+//3.3 
+if (hacker1<hacker2){
+    console.log("The driver's name goes first.")
+}
+else if (hacker1>hacker2){
+    console.log("Yo, the navigator goes first definitely.")
+}
+else console.log("What?! You both have the same name?")
